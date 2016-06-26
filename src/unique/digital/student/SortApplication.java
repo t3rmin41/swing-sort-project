d20cee4175078848399076852b184f58822d0bbc
unknown <t33rmin41@gmail.com>
2016-06-26 09:28:29 +0300

    Added Merge sort algorithm from http://howtodoinjava.com/algorithm/merge-sort-java-example/

--- src/unique/digital/student/SortApplication.java
+++ src/unique/digital/student/SortApplication.java
@@ -202,6 +202,51 @@ public class SortApplication {
     }
     
     private StudentStats[] sortMerge(StudentStats[] studentStats) {
+      //If list is empty; no need to do anything
+        if (studentStats.length <= 1) {
+            return studentStats;
+        }
+         
+        //Split the array in half in two parts
+        StudentStats[] first = new StudentStats[studentStats.length / 2];
+        StudentStats[] second = new StudentStats[studentStats.length - first.length];
+        System.arraycopy(studentStats, 0, first, 0, first.length);
+        System.arraycopy(studentStats, first.length, second, 0, second.length);
+         
+        //Sort each half recursively
+        sortMerge(first);
+        sortMerge(second);
+         
+        //Merge both halves together, overwriting to original array
+        merge(first, second, studentStats);
         return studentStats;
+    }
+    
+    private StudentStats[] merge(StudentStats[] first, StudentStats[] second, StudentStats[] result) {
+        //Index Position in first array - starting with first element
+        int iFirst = 0;
+         
+        //Index Position in second array - starting with first element
+        int iSecond = 0;
+         
+        //Index Position in merged array - starting with first position
+        int iMerged = 0;
+         
+        //Compare elements at iFirst and iSecond, 
+        //and move smaller element at iMerged
+        while (iFirst < first.length && iSecond < second.length) {
+            if (first[iFirst].getScore() > second[iSecond].getScore()) { // compareTo(second[iSecond]) < 0) {
+                result[iMerged] = first[iFirst];
+                iFirst++;
+            } else {
+                result[iMerged] = second[iSecond];
+                iSecond++;
+            }
+            iMerged++;
+        }
+        //copy remaining elements from both halves - each half will have already sorted elements
+        System.arraycopy(first, iFirst, result, iMerged, first.length - iFirst);
+        System.arraycopy(second, iSecond, result, iMerged, second.length - iSecond);
+        return result;
     }
 }
